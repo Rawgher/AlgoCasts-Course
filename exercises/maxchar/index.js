47058492c3ea9cdd5c151bc36466abec3636c11e
Roger Pender <rdpender3@gmail.com>
2022-07-06 15:28:07 -0700

    working through 3 more sections

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,47 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+// Course solution
+function maxChar(str) {
+  let chars = {};
+  let max = 0;
+  let maxChar = '';
+
+  for (let char of str) {
+    chars[char] = ++chars[char] || 1;
+  }
+
+  for (let char in chars) {
+    if (chars[char] > max) {
+      max = chars[char];
+      maxChar = char;
+    }
+  }
+
+  return maxChar;
+}
 
 module.exports = maxChar;
+
+// My initial solution
+// function maxChar(str) {
+//   let chars = {};
+//   let max = '';
+//
+//   for (let char of str) {
+//     // condensed form of if statement from below
+//     chars[char] = ++chars[char] || 1;
+//
+//     // if (!chars[char]) {
+//     //   chars[char] = 1
+//     // } else {
+//     //   ++chars[char]
+//     // }
+//
+//     if (max === '' || chars[char] > chars[max]) {
+//       max = char
+//     }
+//   }
+//
+//   return max;
+// }
--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,11 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  // converting the input to a string to be able to use the reverse function on it, then converting back to a number, then multiplying it by the positive or negative sign
+  // Math.sign() returns 1 or -1 depending on what n is (ex: -51 returns -1 vs 51 returning 1)
+  // could use most of the methods from the reverse string exercise to make this happen
+    return Math.sign(n) * parseInt(n.toString().split('').reverse().join(''));
+}
 
 module.exports = reverseInt;
