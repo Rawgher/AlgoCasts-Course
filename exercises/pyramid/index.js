a14b481018e39311d308bb446cbf9954cfb6acf7
Roger Pender <rdpender3@gmail.com>
2022-07-26 11:20:12 -0700

    working through steps and pyramids

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,43 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n, row = 0, level = '') {
+  if (n === row) {
+    return;
+  }
+
+  if (n + n-1 === level.length) {
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
+
+  let mid = Math.floor((n + n-1)/2);
+
+  let add;
+
+  if (mid - row <= level.length && mid + row >= level.length) {
+    add = '#';
+  } else {
+    add = ' ';
+  }
+
+  pyramid(n, row, level + add);
+}
 
 module.exports = pyramid;
+
+// function pyramid(n) {
+//   let mid = Math.floor((n + n-1)/2);
+//   for (var i = 0; i < n; i++) {
+//       let level = '';
+//
+//       for (var j = 0; j < n + n-1; j++) {
+//         if (mid - i <= j && mid + i >= j) {
+//           level += '#';
+//         } else {
+//           level += ' ';
+//         }
+//       }
+//       console.log(level);
+//     }
+// }
--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,20 +17,46 @@
 //       '### '
 //       '####'
 
-function steps(n) {
+// solution with recursion
+// adding in row and stair to give variables to stop the recursion when needed
+function steps(n, row = 0, stair = '') {
 
-  for (var i = 0; i < n; i++) {
-    let stair = '';
-
-    for (var j = 0; j < n; j++) {
-      if (j <= i) {
-        stair += '#';
-      } else {
-        stair += ' ';
-      }
-    }
-    console.log(stair);
+  // tell the function to stop when n is equal to number of rows
+  if (n === row) {
+    return;
   }
+
+  // tell n to move to the next row when the current stair is finished
+  if (n === stair.length) {
+    console.log(stair);
+    // move on to the next row of the stairs
+    return steps(n, row + 1);
+  }
+
+  if (stair.length <=  row) {
+    stair += '#';
+  } else {
+    stair += ' ';
+  }
+
+  // run the function again to grab the next number on the current stair
+  steps(n, row, stair);
 }
 
 module.exports = steps;
+
+// function steps(n) {
+//
+//   for (var i = 0; i < n; i++) {
+//     let stair = '';
+//
+//     for (var j = 0; j < n; j++) {
+//       if (j <= i) {
+//         stair += '#';
+//       } else {
+//         stair += ' ';
+//       }
+//     }
+//     console.log(stair);
+//   }
+// }
